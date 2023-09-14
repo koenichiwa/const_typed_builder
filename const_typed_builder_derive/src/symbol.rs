851d82bb0b0d1d4cd59619dfe0bc0b8110c2f1f8
Koen van Wel <koenvwel@gmail.com>
2023-09-14 10:08:24 +0200

    Add documentation to generators and symbol

--- const_typed_builder_derive/src/symbol.rs
+++ const_typed_builder_derive/src/symbol.rs
@@ -2,18 +2,29 @@ use proc_macro2::Span;
 use std::fmt::Display;
 use syn::{Ident, Path};
 
+/// A `Symbol` is a wrapper around a string identifier used for constants and identifiers.
 #[derive(Copy, Clone, PartialEq, Eq)]
 pub struct Symbol<'a>(&'a str);
 
+/// Constant representing the "mandatory" symbol.
 pub const MANDATORY: Symbol = Symbol("mandatory");
+/// Constant representing the "group" symbol.
 pub const GROUP: Symbol = Symbol("group");
+/// Constant representing the "builder" symbol.
 pub const BUILDER: Symbol = Symbol("builder");
+/// Constant representing the "single" symbol.
 pub const SINGLE: Symbol = Symbol("single");
+/// Constant representing the "at_least" symbol.
 pub const AT_LEAST: Symbol = Symbol("at_least");
+/// Constant representing the "at_most" symbol.
 pub const AT_MOST: Symbol = Symbol("at_most");
+/// Constant representing the "exact" symbol.
 pub const EXACT: Symbol = Symbol("exact");
+/// Constant representing the "propagate" symbol.
 pub const PROPAGATE: Symbol = Symbol("propagate");
+/// Constant representing the "assume_mandatory" symbol.
 pub const ASSUME_MANDATORY: Symbol = Symbol("assume_mandatory");
+/// Constant representing the "optional" symbol.
 pub const OPTIONAL: Symbol = Symbol("optional");
 
 impl<'a> From<&'a String> for Symbol<'a> {
