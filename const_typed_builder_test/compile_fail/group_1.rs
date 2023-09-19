1c995f6271987221667198f21bd0820ad0ba0d00
koenichiwa <47349524+koenichiwa@users.noreply.github.com>
2023-09-19 17:48:34 +0200

    Update error for group.rs

--- const_typed_builder_test/compile_fail/group_1.rs
+++ const_typed_builder_test/compile_fail/group_1.rs
@@ -4,6 +4,7 @@ use const_typed_builder::Builder;
 fn main() {
     #[derive(Debug, Default, PartialEq, Eq, Builder)]
     #[group(baz = at_least(2))]
+    #[build(solver = brute_force)]
     pub struct Foo {
         #[builder(group = baz)]
         bar: Option<String>,
