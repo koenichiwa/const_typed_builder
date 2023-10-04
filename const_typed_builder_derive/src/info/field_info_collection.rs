dfab2b033e0aad649487bcab5fcceb4addb695b6
koenichiwa <47349524+koenichiwa@users.noreply.github.com>
2023-10-04 16:00:04 +0200

    fmt

--- const_typed_builder_derive/src/info/field_info_collection.rs
+++ const_typed_builder_derive/src/info/field_info_collection.rs
@@ -4,27 +4,29 @@ use super::FieldInfo;
 #[derive(Debug)]
 pub enum FieldInfoCollection<'a> {
     StructFields {
-        fields: Vec<FieldInfo<'a>>
+        fields: Vec<FieldInfo<'a>>,
     },
     EnumFields {
-        variant_fields: BTreeMap<syn::Ident, Vec<FieldInfo<'a>>>
-    }
+        variant_fields: BTreeMap<syn::Ident, Vec<FieldInfo<'a>>>,
+    },
 }
 
-impl <'a> FieldInfoCollection<'a> {
-    pub fn all_fields(&self) -> Vec<&FieldInfo<'a>>{
+impl<'a> FieldInfoCollection<'a> {
+    pub fn all_fields(&self) -> Vec<&FieldInfo<'a>> {
         match self {
             FieldInfoCollection::StructFields { fields } => fields.iter().collect(),
-            FieldInfoCollection::EnumFields { variant_fields } => variant_fields.values().flatten().collect(),
+            FieldInfoCollection::EnumFields { variant_fields } => {
+                variant_fields.values().flatten().collect()
+            }
         }
-        
     }
 
-    pub fn fields_by_variant_ident(&self, variant: syn::Ident) -> Option<&Vec<FieldInfo>> { // TODO: Change to Error
+    pub fn fields_by_variant_ident(&self, variant: syn::Ident) -> Option<&Vec<FieldInfo>> {
+        // TODO: Change to Error
         match self {
             FieldInfoCollection::StructFields { fields } => None,
             FieldInfoCollection::EnumFields { variant_fields } => variant_fields.get(&variant),
-        }   
+        }
     }
 
     pub fn variant_idents(&self) -> Option<impl Iterator<Item = &syn::Ident>> {
