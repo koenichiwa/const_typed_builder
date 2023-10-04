62c1cdec1430a07e52931ba73226279170c3b672
koenichiwa <47349524+koenichiwa@users.noreply.github.com>
2023-10-04 15:59:44 +0200

    fmt

--- const_typed_builder_derive/src/info/mod.rs
+++ const_typed_builder_derive/src/info/mod.rs
@@ -1,9 +1,9 @@
-mod field_info;
-mod group_info;
 mod container_info;
+mod field_info;
 mod field_info_collection;
+mod group_info;
 
+pub use container_info::{ContainerInfo, SolveType};
 pub use field_info::{FieldInfo, FieldKind};
-pub use group_info::{GroupInfo, GroupType};
-pub use container_info::{SolveType, ContainerInfo};
 pub use field_info_collection::FieldInfoCollection;
+pub use group_info::{GroupInfo, GroupType};
